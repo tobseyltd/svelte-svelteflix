deff19f67090a8e86b146616e1eb78cd9b39412e
tobseyltd <mail@tobsey.limited>
2023-08-28 07:55:40 +0200

    created page layout, nav, footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,28 @@
 <script>
+	import logo from '$lib/media/logo.svg';
+	import tmdb from '$lib/media/tmdb.svg';
 	import '../app.postcss';
 </script>
 
 <nav class="flex h-12 items-center justify-between text-primary p-6">
-	<a href="/">Svelteflix</a>
+	<a href="/">
+		<img src={logo} alt="SvelteFlix" class="h-5" />
+	</a>
 
-  <div>
-    <a href="/movies">Movies</a>
-    <a href="/watchlist">Watchlist</a>
-    <a href="/logn">Log in</a>
-  </div>
+	<div class="space-x-4">
+		<a href="/movies">Movies</a>
+		<a href="/watchlist">Watchlist</a>
+		<a href="/logn">Log in</a>
+	</div>
 </nav>
 
-<slot />
+<main>
+	<slot />
+</main>
+
+<footer class="flex justify-center p-6">
+	<div class="text-center">
+		<p>Data provided by</p>
+		<img src={tmdb} alt="tmdb" class="h-3" />
+	</div>
+</footer>
