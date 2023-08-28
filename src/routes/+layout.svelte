bfe31ca8d89a632bfc94e670066a3130b2ecdbc2
tobseyltd <mail@tobsey.limited>
2023-08-28 07:24:57 +0200

    basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
-  import "../app.postcss";
+	import '../app.postcss';
 </script>
+
+<nav>
+	<a href="/">Svelteflix</a>
+
+  <div>
+    <a href="/movies">Movies</a>
+    <a href="/watchlist">Watchlist</a>
+    <a href="/logn">Log in</a>
+  </div>
+</nav>
 
 <slot />
